13032c48e17c7da9e4195794f97a384b88f5cda5
Diego Maroto <diego.diemg@gmail.com>
2026-01-17 14:25:26 +0100

    Extracting player

--- src/main/java/com/marotodiego/hytale/homecrystal/events/ExampleEvent.java
+++ src/main/java/com/marotodiego/hytale/homecrystal/events/ExampleEvent.java
@@ -1,14 +1,23 @@
 package com.marotodiego.hytale.homecrystal.events;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 public class ExampleEvent {
 
     public static void onPlayerReady(PlayerReadyEvent event) {
         Player player = event.getPlayer();
         player.sendMessage(Message.raw("Welcome " + player.getDisplayName()));
+        Ref<EntityStore> ref = event.getPlayerRef();
+        Store<EntityStore> store = ref.getStore();
+
+        PlayerRef playerRef = store.getComponent(ref, PlayerRef.getComponentType());
+        if (playerRef == null) return;
     }
 
 }
